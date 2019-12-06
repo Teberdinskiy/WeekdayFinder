4b73aa9ff48491d168362b7f0016c1f4dab73ded
Alikhan Batchaev <loko-han@mail.ru>
2019-12-06 17:07:31 +0300

    Initial Commit

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by 🧔🏻Alikhan Batchaev on 06.12.2019.
+//  Copyright © 2019 Alikhan Batchaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
